d5274496ddeb582746c92f62271f5e10de434e5d
RahafAlhejaili1607 <Rbalhejaili22@twq.idserve.net>
2023-01-07 17:33:28 +0300

    Initial Commit

---
+++ Taxes calculater/Taxes_calculaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taxes_calculaterApp.swift
+//  Taxes calculater
+//
+//  Created by Rahaf Alhejaili on 14/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Taxes_calculaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
